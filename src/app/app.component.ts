2625a7eb5dd4e5f5c2e38644edb9977c83babd79
= <=>
2021-05-04 16:51:32 +0700

    learn: property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+    <h1
+      [ngStyle]="{color: textColor, background: backgroundColor1}"
+      [ngClass]="{
+        'with-border': withBorder,
+        'other-class': true
+      }"
+    >
+      {{ title }}
+    </h1>
+    <img [src]="imgSrc" />
+  `,
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular';
+  title: string = 'Angula31r';
+  imgSrc: string = "https://picsum.photos/200";
+
+  textColor = '#d23d12';
+  backgroundColor1 = "#f12f11";
+  withBorder = true;
 }
